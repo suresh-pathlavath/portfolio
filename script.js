f7d5a566ed03d9a19dfb3ddd9900e694489cbbd6
Suresh Pathlavath <151118939+suresh-pathlavath@users.noreply.github.com>
2024-10-19 19:44:14 +0530

    pdf friendly on mobile

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function toggleReadMore(event) {
         readMoreLink.textContent = "Read More";
     }
 }
+
+// for certificates pdf
+
+const url = 'https://ik.imagekit.io/suresh29/certificates/analytics_certificates.pdf';
+
+const loadingTask = pdfjsLib.getDocument(url);
+loadingTask.promise.then(pdf => {
+  // Fetch the first page
+  pdf.getPage(1).then(page => {
+    const scale = 1.5;
+    const viewport = page.getViewport({ scale });
+
+    // Prepare canvas using PDF page dimensions
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Append canvas to the viewer
+    document.getElementById('pdf-viewer').appendChild(canvas);
+
+    // Render PDF page into canvas context
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+    };
+    page.render(renderContext);
+  });
+});
